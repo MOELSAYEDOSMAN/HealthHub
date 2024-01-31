d7fb9ecb18d1f261c3a37b9d4dfd48f6da8db0a8
MOELSAYEDOSMAN <dizzy10100961@gmail.com>
2024-01-31 21:35:53 +0200

    Add Docker

--- Backend/HealthHup.API/obj/Debug/net7.0/HealthHup.API.AssemblyInfo.cs
+++ Backend/HealthHup.API/obj/Debug/net7.0/HealthHup.API.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("413247bf-b2e6-4655-b39d-5c87f730cabb")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthHup.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dbd96e9a17ba6bde204db47ac07f45f9dad27ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896bb96186c62caa2ffd21354e5f5dfd44d62d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthHup.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthHup.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
